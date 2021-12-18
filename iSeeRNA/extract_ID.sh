578bc9c9b0ff612904939eca0d8b47b8f68d8d15
imhuangshihui <76728625+imhuangshihui@users.noreply.github.com>
2021-12-18 12:16:22 +0800

    Add files via upload

--- iSeeRNA/extract_ID.sh
+++ iSeeRNA/extract_ID.sh
@@ -2,29 +2,26 @@
 
 source ~/.bashrc
 
-#for i in {1..20};
-#do
-#	cat Rattus_norvegicus.Rnor_6.0.104.chromosome.${i}.gff3 | awk '$3=="lnc_RNA"{print $9}' | cut -d ';' -f1 | cut -d ':' -f2 > ${i}lncrna_id.txt
-
-#done
-
-for i in {1..17};
+for i in {1..20};
 do
-	cat Rattus_norvegicus.Rnor_6.0.104.chromosome.${i}.gff3 | awk '$3=="mRNA"{print $9}' | cut -d ';' -f1 | cut -d ':' -f2 | head -219 > ${i}mrna_id.txt
+	cat Rattus_norvegicus.Rnor_6.0.104.chromosome.${i}.gff3 | awk '$3=="lnc_RNA"{print $9}' | grep 'lincRNA' | cut -d ';' -f1 | cut -d ':' -f2 > ${i}lincRNA_id.txt
 done
 
-for i in {18..20};
+cat Rattus_norvegicus.Rnor_6.0.104.chromosome.X.gff3 | awk '$3=="lnc_RNA"{print $9}' | grep 'lincRNA' | cut -d ';' -f1 | cut -d ':' -f2 > XlincRNA_id.txt
+
+cat Rattus_norvegicus.Rnor_6.0.104.chromosome.Y.gff3 | awk '$3=="lnc_RNA"{print $9}' | grep 'lincRNA' | cut -d ';' -f1 | cut -d ':' -f2 > YlincRNA_id.txt
+
+cat Rattus_norvegicus.Rnor_6.0.104.chromosome.MT.gff3 | awk '$3=="lnc_RNA"{print $9}' | grep 'lincRNA' | cut -d ';' -f1 | cut -d ':' -f2 > MlincRNA_id.txt
+
+cat *lincRNA_id.txt > lincRNA_ID.txt
+
+for i in {1..20};
 do
-	cat Rattus_norvegicus.Rnor_6.0.104.chromosome.${i}.gff3 | awk '$3=="mRNA"{print $9}' | cut -d ';' -f1 | cut -d ':' -f2 | head -218 > ${i}mrna_id.txt
+	cat Rattus_norvegicus.Rnor_6.0.104.chromosome.${i}.gff3 | awk '$3=="mRNA"{print $9}' | cut -d ';' -f1 | cut -d ':' -f2 > ${i}mRNA_id.txt
 done
 
-cat Rattus_norvegicus.Rnor_6.0.104.chromosome.X.gff3 | awk '$3=="mRNA"{print $9}' | cut -d ';' -f1 | cut -d ':' -f2 | head -218 > Xmrna_id.txt
+cat Rattus_norvegicus.Rnor_6.0.104.chromosome.X.gff3 | awk '$3=="mRNA"{print $9}' | cut -d ';' -f1 | cut -d ':' -f2 > XmRNA_id.txt
+cat Rattus_norvegicus.Rnor_6.0.104.chromosome.Y.gff3 | awk '$3=="mRNA"{print $9}' | cut -d ';' -f1 | cut -d ':' -f2 > YmRNA_id.txt
+cat Rattus_norvegicus.Rnor_6.0.104.chromosome.MT.gff3 | awk '$3=="mRNA"{print $9}' | cut -d ';' -f1 | cut -d ':' -f2 > MmRNA_id.txt
 
-cat Rattus_norvegicus.Rnor_6.0.104.chromosome.Y.gff3 | awk '$3=="mRNA"{print $9}' | cut -d ';' -f1 | cut -d ':' -f2 | head -14 > Ymrna_id.txt
-
-cat Rattus_norvegicus.Rnor_6.0.104.chromosome.MT.gff3 | awk '$3=="mRNA"{print $9}' | cut -d ';' -f1 | cut -d ':' -f2 | head -13 > Mmrna_id.txt
-
-cat *mrna_id.txt > mRNA_ID.txt
-
-
-
+cat *mRNA_id.txt > mRNA_ID.txt
